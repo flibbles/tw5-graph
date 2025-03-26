74c89d432d6e62a4d40fb033a42bd146dd2f2a90
Cameron Fischer <fischer.cameron@gmail.com>
2025-03-26 11:28:44 -0400

    Tests to confirm $edge behavior with $to and $from

--- tiddlers/Tests/edge.js
+++ tiddlers/Tests/edge.js
@@ -27,7 +27,32 @@ it("ignores bad numbers for number properties", function() {
 it("can take 'to' node from toTiddler variable", function() {
 	var widget = $tw.test.renderText(wiki, "\\define toTiddler() B\n<$graph><$node $tiddler=A><$node $tiddler=B><$edge $from=A />");
 	expect(Object.values($tw.test.latestEngine.objects.edges)).toEqual([{from: "A", to: "B"}]);
-	
+});
+
+it("updates but doesn't refresh if $from changes", async function() {
+	wiki.addTiddler({title: "from", text: "A"});
+	var widget = $tw.test.renderText(wiki, "<$graph><$node $tiddler=A><$node $tiddler=B><$edge $from={{from}} $to=B />");
+	await $tw.test.flushChanges();
+	var oldEdges = $tw.test.latestEngine.objects.edges;
+	var oldId = Object.keys(oldEdges)[0];
+	expect(oldEdges[oldId]).toEqual({from: "A", to: "B"});
+	wiki.addTiddler({title: "from", text: "B"});
+	await $tw.test.flushChanges();
+	var newEdges = $tw.test.latestEngine.objects.edges;
+	expect(newEdges).toEqual({[oldId]: {from: "B", to: "B"}});
+});
+
+it("updates but doesn't refresh if $to changes", async function() {
+	wiki.addTiddler({title: "to", text: "A"});
+	var widget = $tw.test.renderText(wiki, "<$graph><$node $tiddler=A><$node $tiddler=B><$edge $from=A $to={{to}} />");
+	await $tw.test.flushChanges();
+	var oldEdges = $tw.test.latestEngine.objects.edges;
+	var oldId = Object.keys(oldEdges)[0];
+	expect(oldEdges[oldId]).toEqual({from: "A", to: "A"});
+	wiki.addTiddler({title: "to", text: "B"});
+	await $tw.test.flushChanges();
+	var newEdges = $tw.test.latestEngine.objects.edges;
+	expect(newEdges).toEqual({[oldId]: {from: "A", to: "B"}});
 });
 
 });
