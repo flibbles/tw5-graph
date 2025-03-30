daf164cc8b479ba6b15e6e7d6a3f62e127807568
Cameron Fischer <fischer.cameron@gmail.com>
2025-03-30 14:50:59 -0400

    More documentation on the popup

--- tiddlers/Tests/graph.popup.js
+++ tiddlers/Tests/graph.popup.js
@@ -28,7 +28,6 @@ afterAll(function() {
 	$tw.popup = oldPopup;
 });
 
-// We limit how many of these tests we have, because it sleeps. It's slow.
 it("works with default popup slot", async function() {
 	var expected = '<div class="graph-drop-down"><p>Text content</p></div>';
 	wiki.addTiddler({title: "Target", text: "Text content"});
@@ -52,7 +51,7 @@ it("works with default popup slot", async function() {
 it("works with custom popup slot", async function() {
 	var expected = '<div class="graph-drop-down"><p>Text content</p></div>';
 	wiki.addTiddler({title: "Target", caption: "Text content"});
-	var text =  "<$graph><$graph.popup $ms=0 $state=State>\n\n<$node $tiddler=Target/>\n\n<$fill $name=tooltip>\n\n<$tiddler tiddler=<<currentTooltip>> >\n\n{{!!caption}}";
+	var text =  "<$graph><$graph.popup $ms=0>\n\n<$node $tiddler=Target/>\n\n<$fill $name=tooltip>\n\n<$tiddler tiddler=<<currentTooltip>> >\n\n{{!!caption}}";
 	var widget = $tw.test.renderGlobal(wiki, text);
 	$tw.test.dispatchEvent(wiki, {type: "hover", objectType: "nodes", id: "Target"}, {x: 125, y: 150, xView: 13, yView: 17});
 	// Flush once to make the action-delay trigger
@@ -62,9 +61,24 @@ it("works with custom popup slot", async function() {
 	expect(widget.parentDomNode.innerHTML).toContain(expected);
 	$tw.test.dispatchEvent(wiki, {type: "blur", objectType: "nodes", id: "Target"});
 	await $tw.test.flushChanges();
+	expect(widget.parentDomNode.innerHTML).not.toContain(expected);
+});
+
+it("can be interrupted", async function() {
+	var text = "Text content";
+	wiki.addTiddler({title: "Target", text: text});
+	var text =  "<$graph>\n\n<$graph.popup $ms=0 $state=State>\n\n<$node $tiddler=Target/>\n";
+	var widget = $tw.test.renderGlobal(wiki, text);
+	$tw.test.dispatchEvent(wiki, {type: "hover", objectType: "nodes", id: "Target"}, {x: 125, y: 150, xView: 13, yView: 17});
+	$tw.test.dispatchEvent(wiki, {type: "blur", objectType: "nodes", id: "Target"});
+	expect(wiki.tiddlerExists("State-delay")).toBe(false);
+	// Flush once to make the action-delay trigger
+	await $tw.test.flushChanges();
+	// Flush again to propogate the changed state tiddlers
+	await $tw.test.flushChanges();
 	expect(wiki.tiddlerExists("State")).toBe(false);
 	expect(wiki.tiddlerExists("State-delay")).toBe(false);
-	expect(widget.parentDomNode.innerHTML).not.toContain(expected);
+	expect(widget.parentDomNode.innerHTML).not.toContain(text);
 });
 
 });
