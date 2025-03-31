302ea233482a55e33ae0e67d87d665154a21d55d
Cameron Fischer <fischer.cameron@gmail.com>
2025-03-31 19:29:33 -0400

    Added adapters.js to test engine compliance

---
+++ tiddlers/Tests/adapters.js
@@ -0,0 +1,40 @@
+/*\
+
+Tests that the engines tw5-graph uses comply with what it expects,
+at least for a subset of common actions.
+
+\*/
+
+var adapters = $tw.modules.getModulesByTypeAsHashmap("graphengine");
+
+var Test = adapters.Test;
+
+var doNotTestTheseAdapters = ["Test", "Also", "Orb"];
+
+$tw.utils.each(adapters, function(adapter, name) {
+
+if (doNotTestTheseAdapters.indexOf(name) >= 0) {
+	// We don't test this one. Probably because it's already meant for testing.
+	return;
+}
+
+describe(name + ' Adapter', function() {
+
+it("matches the basic subset of actions", function() {
+	for (var category in Test.properties) {
+		var properties = adapter.properties[category];
+		expect(properties).not.toBeUndefined(category);
+		$tw.utils.each(Test.properties[category], function(property, name) {
+			if (property.type === "actions") {
+				var adapterProperty = properties[name];
+				var blurb = category+"##"+name;
+				expect(adapterProperty).not.toBeUndefined(blurb);
+				expect(adapterProperty.variables || []).withContext(blurb).toEqual(property.variables || []);
+			}
+		});
+	}
+});
+
+});
+
+});
