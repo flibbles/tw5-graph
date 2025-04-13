1144cd77c7f3ee22a6eaa6ed56be864436ff9468
Cameron Fischer <fischer.cameron@gmail.com>
2025-04-12 23:15:29 -0400

    Added $edges.all

---
+++ tiddlers/Tests/edges.all.js
@@ -0,0 +1,88 @@
+/*\
+
+Tests the edges.all macro widget.
+
+\*/
+
+describe('edges.all \\widget', function() {
+
+var wiki, init;
+
+beforeEach(async function() {
+	wiki = new $tw.Wiki();
+	({init} = $tw.test.setSpies());
+	var pluginInfo = $tw.wiki.getPluginInfo("$:/plugins/flibbles/graph");
+	wiki.addTiddlers(Object.values(pluginInfo.tiddlers));
+	wiki.addTiddler($tw.wiki.getTiddler("$:/core/config/GlobalImportFilter"));
+	// TODO: I'd love to not have to do this, but I need a getGraphObjects
+	// that works and doesn't go out of date when I make core plugin changes.
+	await $tw.test.flushChanges();
+});
+
+function fieldConfig(name, properties) {
+	return {
+		title: "$:/config/flibbles/graph/edges/fields/" + name,
+		text: JSON.stringify(properties),
+		type: "application/json"};
+};
+
+function nodesFor() {
+	return Array.prototype.map.call(arguments, x => `<$node $tiddler="${x}"/>`).join("");
+};
+
+it("passes along filter arguments to internal macros", function() {
+	wiki.addTiddlers([
+		fieldConfig("fieldA", {}),
+		fieldConfig("fieldB", {}),
+		{title: "Target", fieldA: "A", fieldB: "B", text: "[[L]] {{T}}"}]);
+	var text = "<$edges.all $formulas=links $fields=fieldB $tiddler=Target><<toTiddler>>";
+	var widget = $tw.test.renderGlobal(wiki, text);
+	expect(widget.parentDomNode.innerHTML).toBe("<p>BL</p>");
+});
+
+it("defaults to defaultTiddler", function() {
+	wiki.addTiddlers([
+		{title: "Target", text: "[[L]]"}]);
+	var text = "\\define currentTiddler() Target\n<$edges.all><<toTiddler>>";
+	var widget = $tw.test.renderGlobal(wiki, text);
+	expect(widget.parentDomNode.innerHTML).toBe("<p>L</p>");
+});
+
+it("passes along edit argument to internal macros", function() {
+	wiki.addTiddlers([
+		fieldConfig("fieldA", {}),
+		{title: "$:/config/TimestampDisable", text: "yes"},
+		{title: "from", fieldA: "field"}]);
+	var text = "<$graph><$edges.all $tiddler=from $editable=yes/>"+nodesFor("from", "field");
+	var widget = $tw.test.renderGlobal(wiki, text);
+	var edges = $tw.test.latestEngine.objects.edges;
+	var id = Object.keys(edges)[0];
+	expect(edges[id]).toEqual({from: "from", to: "field", delete: true});
+	$tw.test.dispatchEvent(wiki, {objectType: "edges", id: id, type: "delete"}, {});
+	expect(wiki.getTiddler("from").fields).toEqual({title: "from"});
+});
+
+it("makes edges for both types by default", function() {
+	wiki.addTiddlers([
+		fieldConfig("fieldA", {value: "A"}),
+		{title: "from", fieldA: "field", text: "[[link]] {{transclude}}"}]);
+	var text = "<$graph><$edges.all $tiddler=from/>" + nodesFor("from", "field", "link", "transclude");
+	var widget = $tw.test.renderGlobal(wiki, text);
+	expect(init).toHaveBeenCalledTimes(1);
+	var objects = init.calls.first().args[1];
+	expect(Object.values(objects.edges)).toEqual([
+		{from: "from", to: "field", value: "A"},
+		{from: "from", to: "link", label: "links to"},
+		{from: "from", to: "transclude", label: "transcludes"}]);
+});
+
+xit("has even depth with its blocks", function() {
+	wiki.addTiddlers([
+		fieldConfig("fieldA", {}),
+		{title: "Target", fieldA: "A", text: "[[L]]"}]);
+	var text = "\\widget $.test() <$edges.all $tiddler=Target><$slot $name=ts-raw $depth=2/>\n<$.test><<toTiddler>>";
+	var widget = $tw.test.renderGlobal(wiki, text);
+	expect(widget.parentDomNode.innerHTML).toBe("<p>AL</p>");
+});
+
+});
