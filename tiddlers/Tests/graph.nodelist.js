467d96165c410dff5f1f803a46133c2cdb3d8e0d
Cameron Fischer <fischer.cameron@gmail.com>
2025-04-21 01:38:48 -0400

    $graph.nodelist works with blocks
    May change the name though

--- tiddlers/Tests/graph.nodelist.js
+++ tiddlers/Tests/graph.nodelist.js
@@ -6,11 +6,11 @@ Tests the nodes.manipulation global widget.
 
 describe('graph.nodelist \\widget', function() {
 
-var wiki, modal, oldModal;
+var wiki, update, modal, oldModal;
 
 beforeEach(async function() {
 	wiki = new $tw.Wiki();
-	$tw.test.setSpies();
+	({update} = $tw.test.setSpies());
 	await $tw.test.setGlobals(wiki);
 	oldModal = $tw.modal;
 	$tw.modal = modal = new $tw.utils.Modal(wiki);
@@ -21,14 +21,41 @@ afterEach(function() {
 	$tw.modal = oldModal;
 });
 
-it("generates a modal when addNode is called", function() {
-	var text = "<$graph><$graph.nodelist $tiddler=Target $field=list>";
+it("generates a modal when addNode is called", async function() {
+	var text = "<$graph><$graph.nodelist $tiddler=Target $field=list/>";
 	var widget = $tw.test.renderGlobal(wiki, text);
 	$tw.test.dispatchEvent(wiki, {objectType: "graph", type: "addNode"}, {x: "37", y: "43"});
 	expect($tw.modal.display).toHaveBeenCalled();
 	$tw.rootWidget.dispatchEvent({type: "tm-modal-finish", param: "newTiddler"});
-	expect(wiki.tiddlerExists("Target")).toBe(true);
 	expect(wiki.tiddlerExists("newTiddler")).toBe(true);
+	expect(wiki.tiddlerExists("Target")).toBe(true);
+	expect(wiki.getTiddler("Target").fields.list).toEqual(["newTiddler"]);
+	await $tw.test.flushChanges();
+	expect(update).toHaveBeenCalledTimes(1);
+	expect(update).toHaveBeenCalledWith({nodes: {newTiddler: {}}});
+});
+
+it("works with custom content block", function() {
+	wiki.addTiddler({title: "Target", list: "A B"});
+	var text = "<$graph.nodelist $tiddler=Target $field=list>{{!!title}}";
+	var widget = $tw.test.renderGlobal(wiki, text);
+	expect(widget.parentDomNode.innerHTML).toBe("<p>AB</p>");
+});
+
+it("combining wth graph.persistent records location", async function() {
+	var text = "<$graph><$graph.persistent $ledgerTiddler=Target><$graph.nodelist $tiddler=Target $field=list/>";
+	var widget = $tw.test.renderGlobal(wiki, text);
+	$tw.test.dispatchEvent(wiki, {objectType: "graph", type: "addNode"}, {x: "37", y: "43"});
+	expect($tw.modal.display).toHaveBeenCalled();
+	$tw.rootWidget.dispatchEvent({type: "tm-modal-finish", param: "newTiddler"});
+	expect(wiki.tiddlerExists("newTiddler")).toBe(true);
+	expect(wiki.tiddlerExists("Target")).toBe(true);
+	expect(wiki.getTiddler("Target").fields.list).toEqual(["newTiddler"]);
+	await $tw.test.flushChanges();
+	expect(update).toHaveBeenCalledTimes(1);
+	var objects = update.calls.first().args[0];
+	expect(objects.nodes.newTiddler.x).toBe(37);
+	expect(objects.nodes.newTiddler.y).toBe(43);
 });
 
 });
