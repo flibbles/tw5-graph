370be44dfa7c2e4f8e9d6b25d5b0bec7705c8985
Cameron Fischer <fischer.cameron@gmail.com>
2025-05-02 00:52:14 -0400

    Taking sidebar seriously; added $boundingbox

---
+++ plugins/graph/widgets/boundingbox.js
@@ -0,0 +1,115 @@
+/*\
+title: $:/plugins/flibbles/graph/widgets/boundingbox.js
+type: application/javascript
+module-type: widget
+
+Widget for creating a DOM element which has strictly controlled dimensions,
+intended for a contained graph.
+
+\*/
+
+"use strict";
+
+var Widget = require("$:/core/modules/widgets/widget.js").widget;
+var utils = require("../utils.js");
+
+var BoxWidget = function(parseTreeNode, options) {
+	this.initialise(parseTreeNode, options);
+	utils.registerForDestruction(this);
+	this.resizeInstance = this.resize.bind(this);
+	this.window = utils.window();
+	this.window.addEventListener("resize", this.resizeInstance);
+};
+
+/*
+Inherit from the base widget class
+*/
+BoxWidget.prototype = new Widget();
+
+/*
+Render this widget into the DOM
+*/
+BoxWidget.prototype.render = function(parent, nextSibling) {
+	this.parentDomNode = parent;
+	this.computeAttributes();
+	this.execute();
+	this.domNode = this.document.createElement("div");
+	this.domNode.className = "boundingbox";
+	parent.insertBefore(this.domNode, nextSibling);
+	// Make sure this comes AFTER inserting domNode into the tree,
+	// otherwise getBoundingClientRect() will be all zeros.
+	this.resize();
+	this.renderChildren(this.domNode, null);
+	this.domNodes.push(this.domNode);
+};
+
+/*
+Compute the internal state of the widget
+*/
+BoxWidget.prototype.execute = function() {
+	this.executeDimensions();
+	this.makeChildWidgets();
+};
+
+BoxWidget.prototype.executeDimensions = function() {
+	var self = this;
+	this.widthFilter = this.wiki.compileFilter(this.getAttribute("$width", ""));
+	this.heightFilter = this.wiki.compileFilter(this.getAttribute("$height", ""));
+	this.dimensionWidget = this.wiki.makeWidget({tree: [{type: "widget", children: []}]}, {parentWidget: this});
+	this.dimensionWidget.execute();
+	// We set up the widget so it only gets values for these variables
+	// if needed. This keeps down unncessary calls to window and document,
+	// and makes the $graph widget usable on Node, if that ever comes up.
+	var variables = {
+		// It may be better to use document.body.clientWidth,
+		// which doesn't consider the scrollbar.
+		windowWidth: function() { return self.window.innerWidth.toString(); },
+		windowHeight: function() { return self.window.innerHeight.toString(); },
+		boundingLeft: function() { return self.domNode.getBoundingClientRect().left.toString(); },
+		boundingTop: function() { return self.domNode.getBoundingClientRect().top.toString(); },
+	};
+	for (var name in variables) {
+		this.dimensionWidget.setVariable(name);
+		Object.defineProperty(this.dimensionWidget.variables[name], "value", { get: variables[name] });
+	}
+};
+
+/*
+Selectively refreshes the widget if needed. Returns true if the widget or any of its children needed re-rendering
+*/
+BoxWidget.prototype.refresh = function(changedTiddlers) {
+	var changedAttributes = this.computeAttributes();
+	var changed = false;
+	if ($tw.utils.count(changedAttributes) > 0) {
+		this.executeDimensions();
+		changed = true;
+	}
+	// We always try to resize.
+	// The $dimension filters might spit out something different.
+	this.resize();
+	return this.refreshChildren(changedTiddlers) || changed;
+};
+
+BoxWidget.prototype.resize = function(event) {
+	var widget = this.dimensionWidget.children[0];
+	var newWidth = this.widthFilter(null, widget)[0] || "";
+	var newHeight = this.heightFilter(null, widget)[0] || "";
+	var style = this.domNode.style;
+	if (newWidth !== style.width) {
+		style.width = newWidth;
+	}
+	if (newHeight !== style.height) {
+		style.height = newHeight;
+	}
+};
+
+BoxWidget.prototype.destroy = function() {
+	this.window.removeEventListener("resize", this.resizeInstance);
+};
+
+BoxWidget.prototype.isGarbage = function() {
+	var body = this.document.body;
+	return !body || !body.contains(this.domNode);
+};
+
+exports.boundingbox = BoxWidget;
---
+++ tiddlers/Tests/boundingbox.js
@@ -0,0 +1,152 @@
+/*\
+
+Tests the boundingbox widget.
+
+\*/
+
+describe('BoundingBoxWidget', function() {
+
+var wiki, register, window;
+
+beforeEach(function() {
+	wiki = new $tw.Wiki();
+	({register, window} = $tw.test.setSpies());
+});
+
+function clean(objects) {
+	objects.edges = Object.values(objects.edges);
+	return objects;
+};
+
+function onlyCallOf(spy) {
+	expect(spy).toHaveBeenCalledTimes(1);
+	return spy.calls.first().args;
+};
+
+/*** garbage handling ***/
+
+it("detects when to destroy itself", async function() {
+	register.and.callThrough();
+	wiki.addTiddler({title: "View", text: "yes"});
+	var widgetNode = $tw.test.renderText(wiki, "<%if [{View}!match[no]] %><$boundingbox />");
+	widgetNode.document.body = {
+		contains: function(node) {
+			while (node) {
+				if (node === widgetNode.parentDomNode) {
+					return true;
+				}
+				// Now check and make sure the parent has this as a child.
+				if (!node.parentNode
+				|| node.parentNode.childNodes.indexOf(node) < 0) {
+					return false;
+				}
+				node = node.parentNode;
+			}
+		}
+	};
+	await $tw.test.flushChanges();
+	$tw.test.utils.upkeep();
+	expect(register).toHaveBeenCalled();
+	register.calls.reset();
+	// Now we put in a change that will
+	wiki.addTiddler({title: "View", text: "no"});
+	await $tw.test.flushChanges();
+	$tw.test.utils.upkeep();
+	expect(window().eventListeners.size).toBe(0);
+});
+
+/*** dimensions ***/
+
+it("resizes on literal dimension changes", async function() {
+	wiki.addTiddler({title: "dimensions", text: "247"});
+	var widgetNode = $tw.test.renderText(wiki, "<$boundingbox $height={{dimensions}} $width={{dimensions}}/>");
+	await $tw.test.flushChanges();
+	expect(widgetNode.parentDomNode.innerHTML).toContain("height:247");
+	expect(widgetNode.parentDomNode.innerHTML).toContain("width:247");
+	wiki.addTiddler({title: "dimensions", text: "300"});
+	await $tw.test.flushChanges();
+	// We want to update the dimensions, but not refresh the graph
+	expect(widgetNode.parentDomNode.innerHTML).toContain("height:300");
+	expect(widgetNode.parentDomNode.innerHTML).toContain("width:300");
+});
+
+it("resizes on filter dimension changes", async function() {
+	wiki.addTiddler({title: "dimensions", text: "27"});
+	var widgetNode = $tw.test.renderText(wiki, "\\function .D() [{dimensions}]\n<$boundingbox $height='[<.D>]' $width='[<.D>add[10]]'>");
+	await $tw.test.flushChanges();
+	expect(widgetNode.parentDomNode.innerHTML).toContain("width:37;height:27;");
+	wiki.addTiddler({title: "dimensions", text: "30"});
+	await $tw.test.flushChanges();
+	expect(widgetNode.parentDomNode.innerHTML).toContain("width:40;height:30;");
+});
+
+it("can remove dimension attributes", async function() {
+	wiki.addTiddler({title: "dimensions", text: "27"});
+	var widgetNode = $tw.test.renderText(wiki, "<$boundingbox $height={{dimensions}} $width={{dimensions}} />\n\n");
+	await $tw.test.flushChanges();
+	expect(widgetNode.parentDomNode.innerHTML).toContain("width:27;height:27;");
+	wiki.addTiddler({title: "dimensions"});
+	await $tw.test.flushChanges();
+	// There will still be style="height:;width:;", but I don't know how to
+	// get rid of that.
+	expect(widgetNode.parentDomNode.innerHTML).not.toContain(":27;");
+});
+
+it("can have dimension attributes return nothing", async function() {
+	wiki.addTiddler({title: "dimensions", text: "27"});
+	var widgetNode = $tw.test.renderText(wiki, "<$boundingbox $height='[{dimensions}]' $width='[{dimensions}]' />\n");
+	await $tw.test.flushChanges();
+	expect(widgetNode.parentDomNode.innerHTML).toContain("width:27;height:27;");
+	wiki.addTiddler({title: "dimensions"});
+	await $tw.test.flushChanges();
+	// There will still be style="height:;width:;", but I don't know how to
+	// get rid of that.
+	expect(widgetNode.parentDomNode.innerHTML).not.toContain(":27;");
+});
+
+it("does not write any style info if no dimensions supplied", function() {
+	function render(text) {
+		return wiki.renderText("text/html", "text/vnd.tiddlywiki", text);
+	};
+	expect(render("<$boundingbox/>\n\n")).toBe('<div class="boundingbox"></div>');
+	expect(render("<$boundingbox $width='' $height=''/>\n\n")).toBe('<div class="boundingbox"></div>');
+	expect(render("<$boundingbox $width=5px/>\n\n")).toBe('<div class="boundingbox" style="width:5px;"></div>');
+	expect(render("<$boundingbox $height=5px/>\n\n")).toBe('<div class="boundingbox" style="height:5px;"></div>');
+	// Has a filter, but filter returns nothing
+	expect(render("<$boundingbox $width='[match[x]]' $height='[match[x]]'/>\n")).toBe('<div class="boundingbox"></div>');
+	expect(render("<$boundingbox $width='[[]]' $height='[[]]'/>\n")).toBe('<div class="boundingbox"></div>');
+});
+
+it("can use browser info for dimension attributes", function() {
+	var createElement = $tw.fakeDocument.createElement;
+	spyOn($tw.fakeDocument, "createElement").and.callFake(function() {
+		var element = createElement.apply(this, arguments);
+		element.getBoundingClientRect = () => ({top: 17, left: 13});
+		return element;
+	});
+	function test(text, expected) {
+		var output = wiki.renderText("text/html", "text/vnd.tiddlywiki", text);
+		expect(output).toContain(expected);
+	};
+	var win = window();
+	win.innerWidth = 53;
+	win.innerHeight = 34;
+	test("<$boundingbox $width='[<windowWidth>]'/>\n", "width:53;");
+	test("<$boundingbox $height='[<windowHeight>]'/>\n", "height:34;");
+	test("<$boundingbox $width='[<boundingLeft>]'/>\n", "width:13;");
+	test("<$boundingbox $height='[<boundingTop>]'/>\n", "height:17;");
+});
+
+it("handles resize events", function() {
+	var win = window();
+	win.innerWidth = 53;
+	win.innerHeight = 34;
+	var widgetNode = $tw.test.renderText(wiki, "<$boundingbox $height='[<windowHeight>]' $width='[<windowWidth>]' />\n");
+	expect(widgetNode.parentDomNode.innerHTML).toContain("width:53;height:34;");
+	win.innerWidth = 7;
+	win.innerHeight = 11;
+	win.dispatchEvent({type: "resize"});
+	expect(widgetNode.parentDomNode.innerHTML).toContain("width:7;height:11;");
+});
+
+});
