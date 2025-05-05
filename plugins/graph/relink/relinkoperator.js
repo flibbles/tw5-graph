0abd2f0bfba7f8417cd3f6b030d677013f687377
Cameron Fischer <fischer.cameron@gmail.com>
2025-05-05 16:22:01 -0400

    Complete testing for $:/graph relinking

--- plugins/graph/relink/relinkoperator.js
+++ plugins/graph/relink/relinkoperator.js
@@ -25,7 +25,7 @@ exports.report = function(tiddler, callback, options) {
 exports.relink = function(tiddler, fromTitle, toTitle, changes, options) {
 	var title = tiddler.fields.title;
 	if ($tw.utils.startsWith(title, "$:/graph/")) {
-		var data = options.wiki.getTiddlerData(title, {});
+		var data = options.wiki.getTiddlerDataCached(title, {});
 		if (data[fromTitle] !== undefined) {
 			var newData = {};
 			for (var title in data) {
@@ -36,12 +36,36 @@ exports.relink = function(tiddler, fromTitle, toTitle, changes, options) {
 				}
 			}
 			var newText;
-			if (tiddler.fields.type === "application/json") {
+			var type = tiddler.fields.type;
+			if (type === "application/x-tiddler-dictionary") {
+				if (isLegalDictionaryKey(toTitle)) {
+					newText = $tw.utils.makeTiddlerDictionary(newData);
+				} else {
+					type = "application/json";
+					changes.type = {output: type};
+				}
+			}
+			if (type === "application/json") {
 				newText = JSON.stringify(newData, null, $tw.config.preferences.jsonSpaces);
-			} else {
-				newText = $tw.utils.makeTiddlerDictionary(newData);
 			}
 			changes.text = {output: newText};
 		}
 	}
+};
+
+function isLegalDictionaryKey(title) {
+	// We make a microdictionary with the value and make sure we get back
+	// a dictionary with the key pointing to an arbitrary x
+	var fields = $tw.utils.parseFields(title + ":x");
+	var found = false;
+	for (var key in fields) {
+		if (found
+		|| key !== title
+		|| fields[key] !== "x") {
+			return false;
+		}
+		found = true;
+
+	}
+	return found;
 };
--- tiddlers/Tests/relink.js
+++ tiddlers/Tests/relink.js
@@ -6,6 +6,8 @@ Tests all relinking.
 
 describe("Relink", function() {
 
+var dictType = "application/x-tiddler-dictionary";
+
 function testTiddler(tiddler, expected, report, options) {
 	const wiki = new $tw.Wiki();
 };
@@ -26,7 +28,7 @@ it("reports dictionary graph tiddlers", function() {
 	const wiki = new $tw.Wiki();
 	var data = "A: 3,5\nB: value";
 	var title = "$:/graph/view/test";
-	wiki.addTiddler({title: title, type: "application/x-tiddler-dictionary", text: data});
+	wiki.addTiddler({title: title, type: dictType, text: data});
 	var report = wiki.getTiddlerRelinkReferences(title);
 	expect(report).toEqual({A: ["3,5"], B: ["value"]});
 	// Reports must be soft, so these don't show up as "missing" tiddlers.
@@ -34,19 +36,74 @@ it("reports dictionary graph tiddlers", function() {
 	expect(wiki.filterTiddlers(`[[${title}]relink:references:hard[]]`)).toEqual([]);
 });
 
-// TODO: Doesn't relink or report on non $:/graph data tiddlers
-// TODO: Empty value don't stop relinking (falsy values)
-// TODO: Leaves non data tiddlers in the namespace alone
-// TODO: Maintains pretty-printing
-
 it("relinks json graph tiddlers", function() {
 	const wiki = new $tw.Wiki();
-	var json = {"from here": "3,5", B: "value"};
+	var json = {a: "val", "from here": "3,5", g: "val"};
+	var data = JSON.stringify(json);
 	var title = "$:/graph/view/test";
 	spyOn(console, "log");
-	wiki.addTiddler({title: title, type: "application/json", text: JSON.stringify(json)});
+	wiki.addTiddler({title: title, type: "application/json", text: data});
+	wiki.renameTiddler("from here", "to there");
+	expect(wiki.getTiddlerData(title)).toEqual({a: "val", "to there": "3,5", g: "val"});
+	// Maintains pretty printing
+	// Also, maintains same general order
+	expect(wiki.getTiddlerText(title)).toEqual('{\n    "a": "val",\n    "to there": "3,5",\n    "g": "val"\n}');
+});
+
+it("relinks dictionary graph tiddlers", function() {
+	const wiki = new $tw.Wiki();
+	var data = "from here: 3,5\nB: value";
+	var title = "$:/graph/view/test";
+	spyOn(console, "log");
+	wiki.addTiddler({title: title, type: dictType, text: data});
 	wiki.renameTiddler("from here", "to there");
 	expect(wiki.getTiddlerData(title)).toEqual({"to there": "3,5", B: "value"});
+});
+
+it("relinks impossible dictionary graph tiddlers with json", function() {
+	const wiki = new $tw.Wiki();
+	var data = "from here: 3,5\nB: value";
+	var title = "$:/graph/view/test";
+	spyOn(console, "log");
+	wiki.addTiddler({title: title, type: dictType, text: data});
+	wiki.renameTiddler("from here", "to:there");
+	expect(wiki.getTiddlerData(title)).toEqual({"to:there": "3,5", B: "value"});
+	expect(wiki.getTiddler(title).fields.type).toBe("application/json");
+});
+
+it("empty values do not stop relinking", function() {
+	const wiki = new $tw.Wiki();
+	var json = {"from here": ""};
+	var data = JSON.stringify(json);
+	var title = "$:/graph/view/test";
+	spyOn(console, "log");
+	wiki.addTiddler({title: title, type: "application/json", text: data});
+	wiki.renameTiddler("from here", "to there");
+	expect(wiki.getTiddlerData(title)).toEqual({"to there": ""});
+});
+
+it("does not touch data tiddlers outside the namespace", function() {
+	const wiki = new $tw.Wiki();
+	var json = {"from here": "3,5"};
+	var data = JSON.stringify(json);
+	var title = "$:/notgraph/view/test";
+	wiki.addTiddler({title: title, type: "application/json", text: data});
+	wiki.renameTiddler("from here", "to there");
+	expect(wiki.getTiddlerData(title)).toEqual({"from here": "3,5"});
+	// It also does not report anything
+	expect(wiki.getTiddlerRelinkReferences(title)).toEqual({});
+});
+
+it("does not touch non-data tiddlers inside the namespace", function() {
+	const wiki = new $tw.Wiki();
+	var json = {"from here": "3,5"};
+	var data = JSON.stringify(json);
+	var title = "$:/graph/view/test";
+	wiki.addTiddler({title: title, type: "text/vnd.tiddlywiki", text: data});
+	wiki.renameTiddler("from here", "to there");
+	expect(wiki.getTiddlerText(title)).toEqual(data);
+	// It also does not report anything
+	expect(wiki.getTiddlerRelinkReferences(title)).toEqual({});
 });
 
 });
