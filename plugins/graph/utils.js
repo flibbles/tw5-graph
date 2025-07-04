e8dcc32e5a43102cb4325ce86b765cf60a09949b
Cameron Fischer <fischer.cameron@gmail.com>
2025-07-03 20:24:40 -0400

    Moved WidgetIterator to utils
    This is the start of a big refactor of the properties system.

--- plugins/graph/utils.js
+++ plugins/graph/utils.js
@@ -91,3 +91,39 @@ exports.upkeep = function() {
 		upkeepId = null;
 	}
 };
+
+exports.WidgetIterator = function(root) {
+	this.stack = [];
+	this.ptr = root;
+	while (this.ptr.children && this.ptr.children.length > 0) {
+		this.stack.push(0);
+		this.ptr = this.ptr.children[0];
+	}
+};
+
+exports.WidgetIterator.prototype.next = function() {
+	var rtn, ptr = this.ptr;
+	if (!ptr) {
+		rtn = {done: true};
+	} else {
+		rtn = {value: ptr, done: false};
+		ptr = ptr.parentWidget;
+		var index = this.stack.pop();
+		if (index !== undefined) {
+			index++;
+			if (ptr && ptr.children.length > index) {
+				ptr = ptr.children[index];
+				this.stack.push(index);
+				// Now dive to the lowest child
+				while (ptr.children && ptr.children.length > 0) {
+					this.stack.push(0);
+					ptr = ptr.children[0];
+				}
+			}
+		} else {
+			ptr = null;
+		}
+	}
+	this.ptr = ptr;
+	return rtn;
+};
--- plugins/graph/widgets/properties.js
+++ plugins/graph/widgets/properties.js
@@ -173,7 +173,7 @@ Properties.prototype.updateGraphWidgets = function(parentCallback) {
 };
 
 Properties.prototype.collectGraphProperties = function(properties) {
-	var iterator = new WidgetIterator(this);
+	var iterator = new utils.WidgetIterator(this);
 	var results;
 	while (!(results = iterator.next()).done) {
 		var widget = results.value;
@@ -199,7 +199,7 @@ Properties.prototype.catchGraphEvent = function(graphEvent, triggeringWidget, va
 };
 
 Properties.prototype.invokeGraphActions = function(graphEvent, variables) {
-	var iterator = new WidgetIterator(this);
+	var iterator = new utils.WidgetIterator(this);
 	var results;
 	while (!(results = iterator.next()).done) {
 		var widget = results.value;
@@ -220,42 +220,5 @@ function propertiesChanged(changedAttributes) {
 	}
 	return false;
 };
-
-function WidgetIterator(root) {
-	this.stack = [];
-	this.ptr = root;
-	while (this.ptr.children && this.ptr.children.length > 0) {
-		this.stack.push(0);
-		this.ptr = this.ptr.children[0];
-	}
-};
-
-WidgetIterator.prototype.next = function() {
-	var rtn, ptr = this.ptr;
-	if (!ptr) {
-		rtn = {done: true};
-	} else {
-		rtn = {value: ptr, done: false};
-		ptr = ptr.parentWidget;
-		var index = this.stack.pop();
-		if (index !== undefined) {
-			index++;
-			if (ptr && ptr.children.length > index) {
-				ptr = ptr.children[index];
-				this.stack.push(index);
-				// Now dive to the lowest child
-				while (ptr.children && ptr.children.length > 0) {
-					this.stack.push(0);
-					ptr = ptr.children[0];
-				}
-			}
-		} else {
-			ptr = null;
-		}
-	}
-	this.ptr = ptr;
-	return rtn;
-};
-
 
 exports.properties = Properties;
