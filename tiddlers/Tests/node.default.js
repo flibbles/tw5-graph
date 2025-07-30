5d12203d8bd588a10474e77c6f957e89d77ac209
Cameron Fischer <fischer.cameron@gmail.com>
2025-07-30 13:10:32 -0400

    Created $node.default

---
+++ tiddlers/Tests/node.default.js
@@ -0,0 +1,32 @@
+/*\
+
+Tests the node.default global widget.
+
+\*/
+
+describe('node.default \\widget', function() {
+
+var wiki, init, update;
+
+beforeEach(async function() {
+	wiki = new $tw.Wiki();
+	({init, update} = $tw.test.setSpies());
+	await $tw.test.setGlobals(wiki);
+});
+
+it("properly sets label using title or caption", function() {
+	// Testing here that the caption gets treated as wikitext,
+	// but the title gets treated as plaintext.
+	wiki.addTiddlers([
+		{title: "{{Transclude}}"},
+		{title: "B", caption: "{{Transclude}}"},
+		{title: "Transclude", text: "label"}]);
+	var text = "<$graph><$list filter='[[{{Transclude}}]] B'><$node.default />";
+	var widget = $tw.test.renderGlobal(wiki, text);
+	var keys = Object.keys(init.calls.first().args[1].nodes);
+	expect(init.calls.first().args[1].nodes).toEqual({
+		"{{Transclude}}": {label: "{{Transclude}}"},
+		B: {label: "label"}});
+});
+
+});
