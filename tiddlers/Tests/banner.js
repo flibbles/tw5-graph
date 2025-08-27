3056e8620787e1983780962972180598bf1ab503
Cameron Fischer <fischer.cameron@gmail.com>
2025-08-27 18:03:52 -0400

    Issue #62: Tests to confirm fullscreen modal behavior

---
+++ tiddlers/Tests/banner.js
@@ -0,0 +1,40 @@
+/*\
+
+Tests the boundingbox widget.
+
+\*/
+
+describe('Banner', function() {
+
+var wiki, register, init, window;
+
+beforeEach(async function() {
+	wiki = new $tw.Wiki();
+	({register, init, window} = $tw.test.setSpies());
+	await $tw.test.setGlobals(wiki);
+});
+
+it("changes navigates to modals when in fullscreen mode", async function() {
+	var createElement = $tw.fakeDocument.createElement;
+	createElement: spyOn($tw.fakeDocument, "createElement").and.callFake(function() {
+		var element = createElement.apply(this, arguments);
+		// Defaults so that boundingbox can run without special handling
+		element.getBoundingClientRect = () => ({top: 30, left: 30});
+		return element;
+	})
+	wiki.addTiddlers([
+		{title: "Main", template: "Template"},
+		{title: "Template", text: "Butts Butts Butts\n\n<$graph><$node $tiddler=Target actions='<$action-navigate $to=Target/>'/>"}]);
+	var text = "<$messagecatcher $tm-modal='<$action-test message=tm-modal/>' $tm-navigate='<$action-test message=tm-navigate/>'>\n\n<$transclude $tiddler='$:/plugins/flibbles/graph/ui/SideBar' graph=Main />\n";
+	var widgetNode = $tw.test.renderGlobal(wiki, text);
+	$tw.test.dispatchEvent(wiki, {type: "actions", objectType: "nodes", id: "Target"});
+	expect($tw.test.actionMethod).toHaveBeenCalledTimes(1);
+	expect($tw.test.actionMethod).toHaveBeenCalledWith({message: "tm-navigate"});
+	$tw.test.actionMethod.calls.reset();
+	wiki.addTiddler({title: "$:/config/flibbles/graph/fullscreen", text: "graph-fullscreen"});
+	$tw.test.dispatchEvent(wiki, {type: "actions", objectType: "nodes", id: "Target"});
+	expect($tw.test.actionMethod).toHaveBeenCalledTimes(1);
+	expect($tw.test.actionMethod).toHaveBeenCalledWith({message: "tm-modal"});
+});
+
+});
--- tiddlers/Tests/modules/window.js
+++ tiddlers/Tests/modules/window.js
@@ -14,6 +14,10 @@ var Window = function() {
 
 $tw.utils.extend(Window.prototype, EventTarget.prototype);
 
+// Defaults just so some tests don't always have to bother setting these.
+Window.prototype.innerHeight = 50;
+Window.prototype.innerWidth = 50;
+
 Window.prototype.EventTarget = EventTarget;
 
 exports.Window = Window;
