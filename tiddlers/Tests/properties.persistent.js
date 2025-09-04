2199b36ce552bdf333d06bb5912750ca24cee15f
Cameron Fischer <fischer.cameron@gmail.com>
2025-09-03 21:53:16 -0400

    Explicit TiddlerData tag gets added to non $:/graph ledgers

--- tiddlers/Tests/properties.persistent.js
+++ tiddlers/Tests/properties.persistent.js
@@ -8,6 +8,9 @@ describe('properties.persistent \\widget', function() {
 
 var wiki, init;
 
+var title = "$:/graph/Ledger";
+var tag = "$:/tags/flibbles/graph/TiddlerData";
+
 beforeEach(async function() {
 	wiki = new $tw.Wiki();
 	({init} = $tw.test.setSpies());
@@ -15,50 +18,75 @@ beforeEach(async function() {
 });
 
 it("creates ledger if it does not exist", function() {
-	var text = "<$graph><$properties.persistent $dataTiddler=Ledger><$list filter=A><$node $pos=<<pos>> />";
+	var text = `<$graph><$properties.persistent $dataTiddler='${title}'><$list filter=A><$node $pos=<<pos>> />`;
 	var widget = $tw.test.renderGlobal(wiki, text);
 	expect(init).toHaveBeenCalledTimes(1);
 	var objects = init.calls.first().args[1];
 	expect(objects.nodes).toEqual({A: {free: true, delete: true}});
 	$tw.test.dispatchEvent(wiki, { type: "free", id: "A", objectType: "nodes"},
 		{x: "29", y: "37"});
-	expect(wiki.tiddlerExists("Ledger")).toBe(true);
-	expect(wiki.getTiddlerData("Ledger")).toEqual({A: "29,37"});
+	expect(wiki.tiddlerExists(title)).toBe(true);
+	expect(wiki.getTiddlerData(title)).toEqual({A: "29,37"});
+	expect(wiki.getTiddlersWithTag(tag)).toEqual([]);
 });
 
 it("erases from ledger", function() {
 	wiki.addTiddlers([{title: "A"}, {title: "B"}]);
-	wiki.addTiddler({title: "Ledger", type: "application/x-tiddler-dictionary", text: "A: 13,17\nB: 19,23"});
-	var text = '<$graph><$properties.persistent $dataTiddler=Ledger><$list filter="[[Ledger]indexes[]]"><$node $pos=<<pos>> delete="<$action-deletetiddler $tiddler=<<currentTiddler>> />" />';
+	wiki.addTiddler({title:title, type: "application/x-tiddler-dictionary", text: "A: 13,17\nB: 19,23"});
+	var text = `<$graph><$properties.persistent $dataTiddler='${title}'><$list filter="[[${title}]indexes[]]"><$node $pos=<<pos>> delete="<$action-deletetiddler $tiddler=<<currentTiddler>> />" />`;
 	var widget = $tw.test.renderGlobal(wiki, text);
 	expect(init).toHaveBeenCalledTimes(1);
 	var objects = init.calls.first().args[1];
 	expect(objects.nodes).toEqual({A: {free: true, delete: true, x: 13, y: 17}, B: {free: true, delete: true, x: 19, y: 23}});
 	$tw.test.dispatchEvent(wiki, { type: "delete", id: "A", objectType: "nodes"});
-	expect(wiki.getTiddlerData("Ledger")).toEqual({B: "19,23"});
+	expect(wiki.getTiddlerData(title)).toEqual({B: "19,23"});
 	expect(wiki.tiddlerExists("A")).toBe(false);
+	expect(wiki.getTiddlersWithTag(tag)).toEqual([]);
 });
 
 it("reads from ledger", function() {
-	wiki.addTiddler({title: "Ledger", type: "application/x-tiddler-dictionary", text: "A: 13,17"});
-	var text = "<$graph><$properties.persistent $dataTiddler=Ledger><$list filter=A><$node $pos=<<pos>> />";
+	wiki.addTiddler({title: title, type: "application/x-tiddler-dictionary", text: "A: 13,17"});
+	var text = `<$graph><$properties.persistent $dataTiddler='${title}'><$list filter=A><$node $pos=<<pos>> />`;
 	var widget = $tw.test.renderGlobal(wiki, text);
 	expect(init).toHaveBeenCalledTimes(1);
 	var objects = init.calls.first().args[1];
 	expect(objects.nodes).toEqual({A: {free: true, delete: true, x: 13, y: 17}});
+	expect(wiki.getTiddlersWithTag(tag)).toEqual([]);
 });
 
 it("does not need currentTiddler to be set to the node", function() {
-	wiki.addTiddler({title: "Ledger", type: "application/json"});
-	var text = "\\procedure currentTiddler() WRONG\n<$graph><$properties.persistent $dataTiddler=Ledger><$node $tiddler=A $pos=<<pos>> />";
+	wiki.addTiddler({title: title, type: "application/json"});
+	var text = `\\procedure currentTiddler() WRONG\n<$graph><$properties.persistent $dataTiddler='${title}'><$node $tiddler=A $pos=<<pos>> />`;
 	var widget = $tw.test.renderGlobal(wiki, text);
 	expect(init).toHaveBeenCalledTimes(1);
 	var objects = init.calls.first().args[1];
 	expect(objects.nodes).toEqual({A: {free: true, delete: true}});
 	$tw.test.dispatchEvent(wiki, { type: "free", id: "A", objectType: "nodes"},
 		{x: "29", y: "37"});
+	expect(wiki.tiddlerExists(title)).toBe(true);
+	expect(wiki.getTiddlerData(title)).toEqual({A: "29,37"});
+	expect(wiki.getTiddlersWithTag(tag)).toEqual([]);
+});
+
+it("Adds tag if created outside of $:/graph namespace", function() {
+	var text = `<$graph><$properties.persistent $dataTiddler=Ledger><$list filter=A><$node $pos=<<pos>> />`;
+	var widget = $tw.test.renderGlobal(wiki, text);
+	$tw.test.dispatchEvent(wiki, { type: "free", id: "A", objectType: "nodes"},
+		{x: "29", y: "37"});
 	expect(wiki.tiddlerExists("Ledger")).toBe(true);
 	expect(wiki.getTiddlerData("Ledger")).toEqual({A: "29,37"});
+	expect(wiki.getTiddlersWithTag(tag)).toEqual(["Ledger"]);
+});
+
+it("Adds tag if existing tiddler outside of $:/graph namespace", function() {
+	var text = `<$graph><$properties.persistent $dataTiddler=Ledger><$list filter=A><$node $pos=<<pos>> />`;
+	wiki.addTiddler({title: "Ledger", type: "application/json", tags: "Also"});
+	var widget = $tw.test.renderGlobal(wiki, text);
+	$tw.test.dispatchEvent(wiki, { type: "free", id: "A", objectType: "nodes"},
+		{x: "29", y: "37"});
+	expect(wiki.tiddlerExists("Ledger")).toBe(true);
+	expect(wiki.getTiddlerData("Ledger")).toEqual({A: "29,37"});
+	expect(wiki.getTiddler("Ledger").fields.tags).toEqual(["Also", tag]);
 });
 
 });
