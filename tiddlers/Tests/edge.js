a43b6c60d3ccaefa2a19c54c33ac57218ce41e33
Cameron Fischer <fischer.cameron@gmail.com>
2025-09-04 18:20:15 -0400

    Moved edge tests to edge.js

--- tiddlers/Tests/edge.js
+++ tiddlers/Tests/edge.js
@@ -6,12 +6,22 @@ Tests edge widgets.
 
 describe('EdgeWidget', function() {
 
-var wiki, window;
+var wiki, init, window;
 
 beforeEach(function() {
 	wiki = new $tw.Wiki();
-	({window, update} = $tw.test.setSpies());
+	({window, init, update} = $tw.test.setSpies());
 });
+
+function clean(objects) {
+	objects.edges = Object.values(objects.edges);
+	return objects;
+};
+
+function onlyCallOf(spy) {
+	expect(spy).toHaveBeenCalledTimes(1);
+	return spy.calls.first().args;
+};
 
 it("empty-string graph attributes do not count", function() {
 	var widget = $tw.test.renderText(wiki, "<$edge $from=N yes=5 no={{missing}} />");
@@ -98,6 +108,78 @@ it("can preserve existing $id when adding new edges", async function() {
 	expect(newEdges).toEqual({
 		Y: {from: "A", to: "B", value: "2"},
 		Z: {from: "A", to: "B", value: "3"}});
+});
+
+/*** to and from ***/
+
+it('handles updates to edges', async function() {
+	wiki.addTiddlers([
+		{title: "A", tags: "node"},
+		{title: "B", tags: "node", list: "A"},
+		{title: "C", tags: "node"},
+		{title: "D", tags: "node", list: "A B"}]);
+	var widgetNode = $tw.test.renderText(wiki, "<$graph><$list filter='[tag[node]]'><$node /><$list variable=to filter='[list[]]'><$edge $to=<<to>> label={{!!toLabel}} />");
+	await $tw.test.flushChanges();
+	expect(Object.values($tw.test.latestEngine.objects.edges)).toEqual([
+		{from: "B", to: "A"},
+		{from: "D", to: "A"},
+		{from: "D", to: "B"}]);
+	// Now we add and remove a node to the graph
+	wiki.addTiddlers([
+		{title: "B", tags: "node", list: "A", toLabel: "newLabel"},
+		{title: "D", tags: "node", list: "A C"}]);
+	await $tw.test.flushChanges();
+	expect(update).toHaveBeenCalledTimes(1);
+	expect(clean(update.calls.first().args[0])).toEqual({edges: [
+		{from: "B", to: "A", label: "newLabel"},
+		null,
+		{from: "D", to: "C"}]});
+	expect(Object.values($tw.test.latestEngine.objects.edges)).toEqual([
+		{from: "B", to: "A", label: "newLabel"},
+		{from: "D", to: "A"},
+		null,
+		{from: "D", to: "C"}]);
+});
+
+it('handles incomplete edges completed later', async function() {
+	wiki.addTiddlers([
+		{title: "A", tags: "node"},
+		{title: "B"}]);
+	var widgetNode = $tw.test.renderText(wiki, "<$graph><$list filter='[tag[node]]'><$node /></$list><$edge $from=A $to=B />");
+	await $tw.test.flushChanges();
+	var objects = $tw.test.latestEngine.objects;
+	expect(objects.nodes).toEqual({A:{}});
+	expect(objects.edges).toBeUndefined();
+	wiki.addTiddler({title: "B", tags: "node"});
+	await $tw.test.flushChanges();
+	var cleanedArgs = clean(onlyCallOf(update)[0]);
+	expect(cleanedArgs).toEqual({nodes: {B: {}}, edges: [{from: "A", to:"B"}]});
+});
+
+it('handles edge getting incompleted later', async function() {
+	wiki.addTiddlers([
+		{title: "A", tags: "node"},
+		{title: "B", tags: "node"}]);
+	var widgetNode = $tw.test.renderText(wiki, "<$graph><$list filter='[tag[node]]'><$node /></$list><$edge $from=A $to=B />");
+	await $tw.test.flushChanges();
+	var objects = clean($tw.test.latestEngine.objects);
+	expect(objects.nodes).toEqual({A:{}, B:{}});
+	expect(objects.edges).toEqual([{from: "A", to: "B"}]);
+	wiki.addTiddler({title: "B"});
+	await $tw.test.flushChanges();
+	var cleanedArgs = clean(onlyCallOf(update)[0]);
+	expect(cleanedArgs).toEqual({nodes: {B: null}, edges: [null]});
+});
+
+it('does not hand over empty edge lists', function() {
+	wiki.addTiddler({title: "A"});
+	var widgetNode = $tw.test.renderText(wiki, "<$graph><$node $tiddler=A/><$edge $from=A $to=B />");
+	expect(init).toHaveBeenCalledTimes(1);
+	// Might expect to have an edge object because one was added,
+	// and then trimmed. But we should be better than that.
+	var objects = init.calls.first().args[1];
+	expect(objects.nodes).toEqual({A: {}});
+	expect(objects.edges).toBeUndefined();
 });
 
 });
